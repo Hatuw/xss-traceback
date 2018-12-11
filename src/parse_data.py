858fda1a4a5ba948a3bf7f797bb21711da81892a
hatuw <jiaxi_wu@ieee.org>
2018-12-11 23:10:26 +0800

    encode data, prepare for training

--- src/parse_data.py
+++ src/parse_data.py
@@ -1,11 +1,15 @@
 # coding: utf-8
 import os
 import re
+import gensim
+from itertools import chain
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 # set global vars
 DATA_DIR = '../data'
-mirror_urls_file = 'mirror_demo.csv'
+# mirror_urls_file = 'mirror_demo.csv'  # toy dataset
+mirror_urls_file = 'xss.csv'  # full dataset
 
 
 def load_data():
@@ -15,9 +19,8 @@ def load_data():
             pruned_data: <pandas.DataFrame>
     """
     file_path = os.path.join(DATA_DIR, mirror_urls_file)
-    assert os.path.exists(file_path), "file not exist"
+    assert os.path.exists(file_path), "file \"{}\" not exist".format(file_path)
     data = pd.read_csv(file_path)
-
     # get data columns
     columns = data.columns
     if 'Author' in columns and 'URL' in columns:
@@ -26,13 +29,23 @@ def load_data():
     else:
         exit(1)
 
+    # 发现数据部分`URL`为nan，返回前先进行处理
+    shape_bf = pruned_data.shape[0]
+    print("[info] Total loaded {} lines of data.".format(shape_bf))
+
+    pruned_data.dropna(inplace=True)
+
+    shape_af = pruned_data.shape[0]
+    print("[info] Remove {} lines of (nan) data, Validated {} lines of data."
+          .format(shape_bf - shape_af, shape_af))
+
     return pruned_data
 
 
 def split_urls(urls):
     """
         split urls
-        param:
+        params:
             urls: string[]
         @return:
             splited_urls: string[]
@@ -63,20 +76,101 @@ def split_urls(urls):
         return result_urls
 
 
-"""
-# parse data
+def train_d2v(urls, load=False, save_model=False, save_data=False):
+    """
+        trianing doc2vec
+        params:
+            urls: string[]
+            load: boolean, load model?
+            save_model: boolean, save model after training?
+            save_data: boolean, save data after training?
+        @return:
+            encoded_urls: <pandas.DataFrame>
+            model: <class 'gensim.models.doc2vec.Doc2Vec'>
+    """
+
+    model_out_path = os.path.join(DATA_DIR, "word2vec.model")
+    data_out_path = os.path.join(DATA_DIR, "train.csv")
+    model = None
+    if load and os.path.exists(model_out_path):
+        model = gensim.models.Doc2Vec.load(model_out_path)
+        print("[info] Successfully loaded word2vec model.")
+
+    if not model:
+        # if model not loaded/exist, re-train the Doc2Vec model
+        # construct training data for `doc2vec`
+        X_training = []
+        for idx, url in enumerate(urls):
+            document = gensim.models.doc2vec.TaggedDocument(url, tags=[idx])
+            X_training.append(document)
+
+        print("[info] Training word2vec model...")
+        model = gensim.models.Doc2Vec(X_training, min_count=1, workers=4)
+        model.train(X_training, total_examples=model.corpus_count, epochs=10)
+
+    # transfer urls to vector
+    encoded_urls = []
+    for url in urls:
+        encoded_urls.append(model.infer_vector(url))
+    encoded_urls = pd.DataFrame(encoded_urls)
+
+    # save model
+    if save_model:
+        model.save(model_out_path)
+        print("[info] Export model to {}.".format(model_out_path))
+
+    # save data
+    if save_data:
+        encoded_urls.to_csv(data_out_path, index=None, header=None)
+        print("[info] Export url data to {}.".format(data_out_path))
+
+    return encoded_urls, model
+
+
+def encode_authors(authors, save_data=False):
+    """
+        encode authors by one-hot encoding
+    """
+    map_out_path = os.path.join(DATA_DIR, "labels_map.csv")
+    data_out_path = os.path.join(DATA_DIR, "labels.csv")
+
+    encoder = LabelEncoder()
+    encoder.fit(authors)  # fit model
+    encoded_data = encoder.transform(authors)  # encode
+    encoded_data = pd.DataFrame(encoded_data)
+
+    if save_data:
+        # save labels map(the map of number-author)
+        with open(map_out_path, 'w') as f_out:
+            for cls_ in encoder.classes_:
+                f_out.write(cls_ + "\n")
+
+        # save encoded data
+        encoded_data.to_csv(data_out_path, index=None, header=None)
+        print("[info] Export labels to {}.".format(data_out_path))
+
+    return encoded_data
+
+
+# load, parse and export word vectors
 def main():
     # load data
     pruned_data = load_data()
-    assert 'URL' in pruned_data.columns, 'data format error'
+
+    # parse urls data
+    assert 'URL' in pruned_data.columns, '`URL` must in columns'
     pruned_urls = split_urls(pruned_data['URL'])
 
-    for i in range(5):
-        # if len(pruned_urls[i]) < 30:
-        print(pruned_data['URL'][i])
-        print(pruned_urls[i])
+    # trian word2vec
+    # encoded_urls, _ = train_d2v(pruned_urls,
+    #                             load=True,
+    #                             save_model=True,
+    #                             save_data=True)
+
+    # parse author data
+    assert 'Author' in pruned_data.columns, '`Author` must in columns'
+    authors = encode_authors(pruned_data['Author'], save_data=True)
 
 
 if __name__ == '__main__':
     main()
-"""
